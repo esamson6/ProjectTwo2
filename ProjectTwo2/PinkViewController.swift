53b9e5ef5db52bfd6361abc2e54e33a9f7a0cc6b
esamson6 <erin.samson34@yahoo.com>
2021-03-28 01:51:21 -1000

    Commit

--- ProjectTwo2/PinkViewController.swift
+++ ProjectTwo2/PinkViewController.swift
@@ -9,21 +9,30 @@ import UIKit
 
 class PinkViewController: UIViewController {
 
+    
+    @IBOutlet weak var myNameIsLabel: UILabel!
+    @IBOutlet weak var erinLabel: UILabel!
+    @IBOutlet weak var myMajorIsinLabel: UILabel!
+    @IBOutlet weak var myMajorLabel: UILabel!
+    @IBOutlet weak var springClassesLabel: UILabel!
+    
+    
     override func viewDidLoad() {
+        
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
-    }
+        
+        myNameIsLabel.text = "My Name is:"
+        erinLabel.text = "Erin"
+        myMajorIsinLabel.text = "My Major is in Creative Media."
+        myMajorLabel.text = "My Major:"
+        springClassesLabel.text = "I have 3 Spring Classes:"
+        
+        
+    
+}
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
+
+
