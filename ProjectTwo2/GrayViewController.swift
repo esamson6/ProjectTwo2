26d2137358a73dd9ad54bab983e832d4b2be5adb
esamson6 <erin.samson34@yahoo.com>
2021-03-28 11:23:01 -1000

    Commit Gray VC

--- ProjectTwo2/GrayViewController.swift
+++ ProjectTwo2/GrayViewController.swift
@@ -9,21 +9,38 @@ import UIKit
 
 class GrayViewController: UIViewController {
 
+    
+    @IBOutlet weak var bucketTableView: UITableView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        bucketTableView.delegate = self
+        bucketTableView.dataSource = self
+        
 
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
     }
-    */
 
+extension GrayViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("You clicked on the cell.")
+        
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = bucketTableView.dequeueReusableCell(withIdentifier: "bucketcell", for: indexPath)
+        cell.textLabel?.text = "My bucket list."
+        return cell
+    }
+    
 }
+
+
